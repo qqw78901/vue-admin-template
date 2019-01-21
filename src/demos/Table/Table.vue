9f64cf731993098d444259d33c7a446a7cfda86d
左文祺 <zuowenqi@yy.com>
2019-01-21 16:00:56 +0800

    添加demo

---
+++ src/demos/Table/Table.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Table",
+        data() {
+            return {
+                key: value
+            }
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>
